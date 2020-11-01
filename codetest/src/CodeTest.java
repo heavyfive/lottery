f8aa7c8bceee097bd155fd790e986ac940cb504d
Heavyfive <heavyfive@gmail.com>
2020-11-02 00:47:07 +0900

    CodeTest 클래스 추가

---
+++ codetest/src/CodeTest.java
@@ -0,0 +1,9 @@
+public class CodeTest {
+
+    public static void main(String [] args) {
+        float floatVal = 1.99999f;
+        int intVal = (int) floatVal;
+
+        System.out.println(intVal);
+    }
+}
